1f0234a6ea9e2219d0f5f5afb75b0f9dc53195dd
Sathyanarayanan <110828645+Sathyanarayanan-R@users.noreply.github.com>
2023-11-10 05:05:37 +0530

    updated

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000' });
+const API = axios.create({ baseURL: 'https://transaction-dashboard-backend-sj.onrender.com' });
 
 export const fetchProductsBySearchAndPagination = (month, search, page) => API.get(`/products/search/${month}?searchQuery=${search || ""}&page=${page}`);
 
